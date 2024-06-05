40324493c057e97fd52313281ac5110ee2db976b
Maxime Leherquer <45520008+lehmax@users.noreply.github.com>
2024-06-05 14:40:37 +0200

    add sidebar

---
+++ src/components/SideBar/index.jsx
@@ -0,0 +1,9 @@
+const SideBar = () => {
+  return (
+    <aside>
+      <small>Copyright, SportSee 2020</small>
+    </aside>
+  )
+}
+
+export default SideBar
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,14 +1,16 @@
 import { Outlet } from 'react-router-dom'
 import Header from '../components/Header'
+import SideBar from '../components/SideBar'
 
 const Layout = () => {
   return (
-    <div>
+    <>
       <Header />
+      <SideBar />
       <main>
         <Outlet />
       </main>
-    </div>
+    </>
   )
 }
 
