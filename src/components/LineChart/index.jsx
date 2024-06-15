a9e889dc63739880d00db2b25eb78dea79c44811
Maxime Leherquer <45520008+lehmax@users.noreply.github.com>
2024-06-15 11:18:34 +0200

    wip

--- src/components/LineChart/index.jsx
+++ src/components/LineChart/index.jsx
@@ -93,89 +93,99 @@ const LineChart = ({ title, data }) => {
   const { marginTop } = dimensions
 
   return (
-    <div className="relative w-full rounded-xl bg-redDark aspect-square">
-      <span className="absolute text-white opacity-50 text- top-10 left-10 max-w-40">
-        {title}
-      </span>
-      <svg
-        className="w-full h-full"
-        onPointerEnter={onMove}
-        onPointerMove={onMove}
-        onPointerLeave={onLeave}
-      >
-        <defs>
-          <linearGradient
-            gradientUnits="userSpaceOnUse"
-            id="gradient"
-            x1="0%"
-            x2="100%"
-            y1="0%"
-            y2="0%"
-          >
-            <stop offset="0%" stopColor="hsl(0deg 0% 100% / 0.3)" />
-            <stop offset="100%" stopColor="white" />
-          </linearGradient>
-        </defs>
-        <g transform={`translate(-10,  ${marginTop})`}>
-          <path
-            d={lineGenerator(extendedData)}
-            fill="none"
-            stroke="url('#gradient')"
-            strokeWidth="3"
-          />
-          {ticks.map(({ value, label, xOffset }) => (
-            <g
-              key={value}
-              transform={`translate(${xOffset}, ${marginTop + 100})`}
-            >
-              <text
-                key={value}
-                style={{
-                  fontSize: '12px',
-                  textAnchor: 'middle',
-                  fill: 'white',
-                }}
-              >
-                {label}
-              </text>
-            </g>
-          ))}
-        </g>
-        <g
-          transform={`translate(-10,  ${marginTop})`}
-          className={!tooltipData.visible ? 'hidden' : ''}
+    <div className="relative w-full overflow-hidden rounded-xl bg-redDark aspect-square">
+      <div
+        style={{ width: `${tooltipData.x - 10}px` }}
+        className={
+          !tooltipData.visible
+            ? 'hidden'
+            : 'absolute top-0 left-0 z-10 h-full bg-red'
+        }
+      />
+      <div className="relative z-20 w-full h-full">
+        <span className="absolute text-white opacity-50 text- top-10 left-10 max-w-40">
+          {title}
+        </span>
+        <svg
+          className="w-full h-full"
+          onPointerEnter={onMove}
+          onPointerMove={onMove}
+          onPointerLeave={onLeave}
         >
-          <circle
-            cx={tooltipData.x}
-            cy={tooltipData.y}
-            r="8"
-            strokeWidth="5"
-            stroke="white"
-            strokeOpacity="50%"
-            fill="white"
-          />
-
-          <rect
-            x={tooltipData.x - 20}
-            y={tooltipData.y - 50}
-            width="40px"
-            height="25px"
-            fill="white"
-            rx="5"
-            ry="5"
-          />
-          <text
-            x={tooltipData.x - 13}
-            y={tooltipData.y - 34}
-            fontFamily="Verdana"
-            fontSize="8px"
-            fontWeight="500"
-            fill="black"
+          <defs>
+            <linearGradient
+              gradientUnits="userSpaceOnUse"
+              id="gradient"
+              x1="0%"
+              x2="100%"
+              y1="0%"
+              y2="0%"
+            >
+              <stop offset="0%" stopColor="hsl(0deg 0% 100% / 0.3)" />
+              <stop offset="100%" stopColor="white" />
+            </linearGradient>
+          </defs>
+          <g transform={`translate(-10,  ${marginTop})`}>
+            <path
+              d={lineGenerator(extendedData)}
+              fill="none"
+              stroke="url('#gradient')"
+              strokeWidth="3"
+            />
+            {ticks.map(({ value, label, xOffset }) => (
+              <g
+                key={value}
+                transform={`translate(${xOffset}, ${marginTop + 100})`}
+              >
+                <text
+                  key={value}
+                  style={{
+                    fontSize: '12px',
+                    textAnchor: 'middle',
+                    fill: 'white',
+                  }}
+                >
+                  {label}
+                </text>
+              </g>
+            ))}
+          </g>
+          <g
+            transform={`translate(-10,  ${marginTop})`}
+            className={!tooltipData.visible ? 'hidden' : ''}
           >
-            {tooltipData.value}
-          </text>
-        </g>
-      </svg>
+            <circle
+              cx={tooltipData.x}
+              cy={tooltipData.y}
+              r="8"
+              strokeWidth="5"
+              stroke="white"
+              strokeOpacity="50%"
+              fill="white"
+            />
+
+            <rect
+              x={tooltipData.x - 20}
+              y={tooltipData.y - 50}
+              width="40px"
+              height="25px"
+              fill="white"
+              rx="5"
+              ry="5"
+            />
+            <text
+              x={tooltipData.x - 13}
+              y={tooltipData.y - 34}
+              fontFamily="Verdana"
+              fontSize="8px"
+              fontWeight="500"
+              fill="black"
+            >
+              {tooltipData.value}
+            </text>
+          </g>
+        </svg>
+      </div>
     </div>
   )
 }
